36a7f9c061a190228393095483f29a0829674299
diogo <diogotairanclaudino@hotmail.com>
2024-03-28 10:13:04 -0300

    adicionado atividade if-else

--- Atividade.java
+++ Atividade.java
@@ -1,5 +1,20 @@
-public class Atividade {
+import java.util.Scanner;
+
+class HelloWorld {
     public static void main(String[] args) {
-        
+
+        try (Scanner sc = new Scanner(System.in)) {
+            int idade = 18;
+
+            System.out.println("Informe sua idade");
+            idade = sc.nextInt();
+
+            if( idade >= 18){
+                System.out.println("Voce é maior de idade");
+            }else{
+                System.out.println("Voce é menor de idade");
+            }
+        }
+
     }
 }
