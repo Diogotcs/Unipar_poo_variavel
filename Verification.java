2b639e22149327b06ff37748e1e376e472c435c1
diogo <diogotairanclaudino@hotmail.com>
2024-03-29 18:02:12 -0300

    🛠️ Verification.java -> Added user authentication logic 🛠️ Verification.java -> Added number comparison logic 🟢 HelloWorld.java -> Added code for age and number verification

--- Verification.java
+++ Verification.java
@@ -1,29 +1,94 @@
 
-//atividade 4        
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int idade = 18;
+
+        System.out.println("Informe sua idade");
+        idade = sc.nextInt();
+
+        if( idade > 18){
+            System.out.println("Voce é maior de idade");
+        }else{
+            System.out.println("Voce é menor de idade");
+        }
+
+    }
+}
+
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int Num = 0;
+
+        System.out.println("Informe um numero");
+        Num = sc.nextInt();
+
+        if( Num % 2 == 0){
+            System.out.println("Numero informado e par");
+        }else{
+            System.out.println("Numero informado e impar");
+        }
+
+    }
+}
+
 import java.util.Scanner;
 
  class Verification {
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            int numero1 = 0;
-            int numero2 = 0;
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Informe um número: ");
-            numero1= scanner.nextInt();
+        String nomeUsuario = "Andrey";
+        String senha = "senha123";
 
-            System.out.println("Informe outro número: ");
-            numero2= scanner.nextInt();
+        System.out.println("Nome do usuario: ");
+        nomeUsuario= scanner.nextLine();
 
-            if( numero1 > numero2 ){
-                System.out.println("O maior número é: "+ numero1);
-            } else if (numero2 > numero1) {
-                System.out.println("O maior número é: "+ numero2);
-            }else  {
-                System.out.println("Os números são iguais.");
-            }
+        System.out.println("Senha: ");
+        senha= scanner.nextLine();
+
+        if( nomeUsuario == "Andrey" && senha == "senha123") {
+            System.out.println("Login bem sucedido");
+        }else{
+            System.out.println("Nome de usuario ou senha incorretos");
         }
 
     }
 }
-    
+
+import java.util.Scanner;
+
+ class Verification {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numero1 = 0;
+        int numero2 = 0;
+
+        System.out.println("Informe um número: ");
+        numero1= scanner.nextInt();
+
+        System.out.println("Informe outro número: ");
+        numero2= scanner.nextInt();
+
+        if( numero1 > numero2 ){
+            System.out.println("O maior número é: "+ numero1);
+        } else if (numero2 > numero1) {
+            System.out.println("O maior número é: "+ numero2);
+        }else  {
+            System.out.println("Os números são iguais.");
+        }
+
+    }
+}
